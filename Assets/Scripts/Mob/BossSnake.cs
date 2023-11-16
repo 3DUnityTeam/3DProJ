4130da868b4371f3de5c2a24121800f7815b43ff
J0Hyejin <141013135+J0Hyejin@users.noreply.github.com>
2023-11-16 16:31:26 +0900

    보스 뱀 제작 완료

--- Assets/Scripts/Mob/BossSnake.cs
+++ Assets/Scripts/Mob/BossSnake.cs
@@ -1,27 +1,27 @@
 using System.Collections;
 using UnityEngine;
 
-public class BossSnake : MonoBehaviour
+public class BossSnake : MobParent
 {
     public GameObject[] Fxs; //Summon, Fire Breath
     public GameObject Player;
+    public GameObject mobSpawn;
     Transform trans_;
     Animator ani_;
 
     public float traceDist = 6.5f;
-    int summons = 50;
+    public int summons = 50;
 
     bool isWatching = false;
-    bool timeFlag = false;
-    bool atkFlag = false;
-
-    bool isFlame = false;
+    bool flag = false;
+    bool isFound = false;
 
     private void Awake()
     {
         trans_ = GetComponent<Transform>();
         ani_ = GetComponent<Animator>();
     }
+
 
     private void Update()
     {
@@ -33,20 +33,51 @@ public class BossSnake : MonoBehaviour
         }
     }
 
-    private void FixedUpdate()
-    {
 
+    IEnumerator FlameShot()
+    {
+        if (!flag)
+        {
+            flag = true;
+            Fxs[1].SetActive(true);
+            ani_.SetBool("Flame", true);
+            yield return new WaitForSeconds(2.2f);
+            Fxs[1].SetActive(false);
+            ani_.SetBool("Flame", false);
+            yield return new WaitForSeconds(5.2f);
+            flag = false;
+        }
     }
 
-    IEnumerator Timmer(float time)
+    IEnumerator Summon()
     {
-        if (!timeFlag && atkFlag)
+        if (!flag)
         {
-            timeFlag = true;
-            yield return new WaitForSeconds(time);
-            isFlame = false;
-            atkFlag = false;
-            timeFlag = false;
+            flag = true;
+            Fxs[0].SetActive(true);
+            ani_.SetBool("Summon", true);
+            for (int i = 0; i < summons; i++)
+            {
+                Debug.Log("4");
+                if (!isFound)
+                    break;
+                else
+                {
+                    float tX = trans_.position.x + Random.Range(-10f, 10f);
+                    float tZ = trans_.position.x + Random.Range(-10f, 10f);
+                    int random = Random.Range(0, GameManager.instance.SpawnManager.pools.Length);
+                    GameObject obj = GameManager.instance.SpawnManager.Get(random);
+                    obj.transform.position = new Vector3(tX, trans_.position.y, tZ);
+                    obj.transform.parent = mobSpawn.transform;
+
+                    summons--;
+                    yield return new WaitForSeconds(0.95f);
+                }
+            }
+            summons = 0;
+            Fxs[0].SetActive(false);
+            ani_.SetBool("Summon", false);
+            flag = false;
         }
     }
 
@@ -56,11 +87,25 @@ public class BossSnake : MonoBehaviour
 
         if (dist <= traceDist)
         {
-            Debug.Log("Snake found player");
-            isWatching = true;
+            isFound = true;
+            if(summons > 0)
+            {
+                StartCoroutine(Summon());
+            }
+            else
+            {
+                isWatching = true;
+                StartCoroutine(FlameShot());
+            }
         }
         else
         {
+            isFound = false;
+            isWatching = false;
+            ani_.SetBool("Summon", false);
+            Fxs[0].SetActive(false);
+            ani_.SetBool("Flame", false);
+            Fxs[1].SetActive(false);
         }
 
         yield return new WaitForSeconds(0.3f);
